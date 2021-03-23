24aa830d2561bb6f9a26369f41a662b6b5ab4768
clampr <christian.lamprecht@aol.de>
2021-03-23 18:34:43 +0100

    Fix module

--- server/__init__.py
+++ server/__init__.py
@@ -5,7 +5,7 @@ Official Meteostat JSON API
 The code is licensed under the MIT license.
 """
 
-__appname__ = 'meteostat-server'
+__appname__ = 'server'
 __version__ = '0.0.1'
 
 from flask import Flask
@@ -14,7 +14,7 @@ from flask import Flask
 app = Flask(__name__)
 
 # Import API endpoints
-from server.station import hourly as station_hourly
-from server.station import daily as station_daily
-from server.point import hourly as point_hourly
-from server.point import daily as point_daily
+from .point import hourly as point_hourly
+from .point import daily as point_daily
+from .station import hourly as station_hourly
+from .station import daily as station_daily
